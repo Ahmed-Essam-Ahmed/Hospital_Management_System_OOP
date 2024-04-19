c6803efa771d6be81f763bbf5bc3f21e5d26090b
OmarElMaraghy <163595422+OmarElMaraghy@users.noreply.github.com>
2024-04-19 23:51:59 +0200

    add and remove beds

--- obj/Debug/net8.0-windows/Hospital_Managment_System_OOP.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/Hospital_Managment_System_OOP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hospital_Managment_System_OOP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+954f40c0b1cd55ab0a6f06c29a69a422c10dab04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75e359f46a73b303ed18fd4a069d8fdd219069cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hospital_Managment_System_OOP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hospital_Managment_System_OOP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
