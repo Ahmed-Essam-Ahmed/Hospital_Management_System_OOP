1e2a83608432d0f7cdd29f14a57d383e88b0f09c
OmarElMaraghy <163595422+OmarElMaraghy@users.noreply.github.com>
2024-04-20 00:44:20 +0200

    Added delete patient to admin

--- Admin_Delete_Patient.Designer.cs
+++ Admin_Delete_Patient.Designer.cs
@@ -30,17 +30,19 @@
         {
             label1 = new Label();
             label2 = new Label();
-            textBox1 = new TextBox();
+            bxDelete = new TextBox();
             button1 = new Button();
             btnHomepage = new Button();
             btnBack = new Button();
+            TablePatient = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)TablePatient).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(92, 65);
+            label1.Location = new Point(12, 9);
             label1.Name = "label1";
             label1.Size = new Size(244, 45);
             label1.TabIndex = 0;
@@ -49,27 +51,29 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(69, 229);
+            label2.Location = new Point(63, 299);
             label2.Name = "label2";
-            label2.Size = new Size(108, 15);
+            label2.Size = new Size(185, 15);
             label2.TabIndex = 1;
-            label2.Text = "Patient ID to Delete";
+            label2.Text = "Patient to Delete(Using Password)";
+            label2.Click += label2_Click;
             // 
-            // textBox1
+            // bxDelete
             // 
-            textBox1.Location = new Point(199, 226);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
-            textBox1.TabIndex = 2;
+            bxDelete.Location = new Point(272, 296);
+            bxDelete.Name = "bxDelete";
+            bxDelete.Size = new Size(100, 23);
+            bxDelete.TabIndex = 2;
             // 
             // button1
             // 
-            button1.Location = new Point(337, 229);
+            button1.Location = new Point(432, 296);
             button1.Name = "button1";
             button1.Size = new Size(75, 23);
             button1.TabIndex = 3;
             button1.Text = "Delete";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // btnHomepage
             // 
@@ -77,7 +81,7 @@
             btnHomepage.Name = "btnHomepage";
             btnHomepage.Size = new Size(172, 23);
             btnHomepage.TabIndex = 4;
-            btnHomepage.Text = "Return to Homepagr";
+            btnHomepage.Text = "Return to Homepage";
             btnHomepage.UseVisualStyleBackColor = true;
             btnHomepage.Click += btnHomepage_Click;
             // 
@@ -91,19 +95,30 @@
             btnBack.UseVisualStyleBackColor = true;
             btnBack.Click += btnBack_Click;
             // 
+            // TablePatient
+            // 
+            TablePatient.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            TablePatient.Location = new Point(99, 70);
+            TablePatient.Name = "TablePatient";
+            TablePatient.Size = new Size(567, 196);
+            TablePatient.TabIndex = 6;
+            // 
             // Admin_Delete_Patient
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(TablePatient);
             Controls.Add(btnBack);
             Controls.Add(btnHomepage);
             Controls.Add(button1);
-            Controls.Add(textBox1);
+            Controls.Add(bxDelete);
             Controls.Add(label2);
             Controls.Add(label1);
             Name = "Admin_Delete_Patient";
             Text = "Admin_Delete_Patient";
+            Load += Admin_Delete_Patient_Load;
+            ((System.ComponentModel.ISupportInitialize)TablePatient).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -112,9 +127,10 @@
 
         private Label label1;
         private Label label2;
-        private TextBox textBox1;
+        private TextBox bxDelete;
         private Button button1;
         private Button btnHomepage;
         private Button btnBack;
+        private DataGridView TablePatient;
     }
 }
--- Admin_Delete_Patient.cs
+++ Admin_Delete_Patient.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -30,5 +31,97 @@ namespace Hospital_Managment_System_OOP
             Login_Page login_Page = new Login_Page();
             login_Page.Show();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Admin_Delete_Patient_Load(object sender, EventArgs e)
+        {
+            DataTable table = new DataTable();
+            table.Columns.Add("Username", typeof(string));
+            table.Columns.Add("Password", typeof(string));
+            TablePatient.DataSource = table;
+
+            string filePath = "PatientData.txt";
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(',');
+
+                    string username = parts[10];
+                    string password = parts[11];
+                    table.Rows.Add(username, password);
+                }
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filePath = "PatientData.txt";
+            string[] lines = File.ReadAllLines(filePath);
+
+            bool deleted = false; // Flag to indicate if any patient was deleted
+
+            // Open the file for writing
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                foreach (string line in lines)
+                {
+                    // Split the line by comma and get the password (assuming it's at index 11)
+                    string[] parts = line.Split(',');
+
+                    string password = parts[11].Trim();
+
+                    // Check if the password matches the one to delete
+                    if (password != bxDelete.Text)
+                    {
+                        // If the password doesn't match, write the line back to the file
+                        sw.WriteLine(line);
+                    }
+                    else
+                    {
+                        // If the password matches, mark as deleted
+                        deleted = true;
+                    }
+
+                }
+            }
+
+            if (deleted)
+            {
+                MessageBox.Show("Patient with the specified password has been deleted.");
+            }
+            else
+            {
+                MessageBox.Show("No patient found with the specified password.");
+            }
+            DataTable table = new DataTable();
+            table.Columns.Add("Username", typeof(string));
+            table.Columns.Add("Password", typeof(string));
+            TablePatient.DataSource = table;
+
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(',');
+
+                    string username = parts[10];
+                    string password = parts[11];
+                    table.Rows.Add(username, password);
+                }
+            }
+        }
     }
 }
+            
+        
